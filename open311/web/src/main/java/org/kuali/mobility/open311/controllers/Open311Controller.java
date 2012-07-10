b8b097b77476d358bae7565ffa63c7698ccee1b4
aashishmittal <ashishmittal.mail@gmail.com>
2012-07-10 21:54:42 +0530

    Modified the Open311 Service domain model and added support for jaxb repository in pom

--- open311/web/src/main/java/org/kuali/mobility/open311/controllers/Open311Controller.java
+++ open311/web/src/main/java/org/kuali/mobility/open311/controllers/Open311Controller.java
@@ -381,7 +381,7 @@ public class Open311Controller {
     public String getServiceAttributes(Model uiModel, @PathVariable("serviceCode") String serviceCode){
     	LOG.debug( "getServiceAttributes() : service code = "+serviceCode );
 		Attributes serviceAttributes = open311Service.getServiceAttributes(serviceCode);
-		uiModel.addAttribute("serviceAttributes", serviceAttributes.getAttributes());
+		uiModel.addAttribute("serviceAttributes", serviceAttributes.getAttributes().getAttribute());
     	return "open311/service";
     }
 
